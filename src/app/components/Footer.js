15428eb56faf2394d93d2d53a4e696eba337329f
xmaxxxx <amritpal29919singh@gmail.com>
2024-12-15 20:00:13 -0500

    `Refactor Footer component: updated class names, spacing, and layout`

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -8,10 +8,10 @@ export const metadata = {
 
 const Footer = () => {
     return (
-        <footer className=" text-white py-10">
+        <footer className="text-white py-10">
             <div className="max-w-6xl mx-auto px-4">
-                <div className="flex justify-between items-center mb-1">
-                    <div className="text-xl font-semibold">
+                <div className="flex flex-col sm:flex-row justify-between items-center mb-6">
+                    <div className="text-xl font-semibold mb-4 sm:mb-0">
                         <span>Amritpal Singh</span>
                     </div>
                     <div className="flex space-x-6">
@@ -24,7 +24,7 @@ const Footer = () => {
                         <Link href="#" target="_blank" className="text-gray-400 hover:text-white">
                             <FaInstagram />
                         </Link>
-                        <Link href="https://www.linkedin.com/in/amritpalsinghx21/" target="_blank" className="text-gray-400 hover:text-white ">
+                        <Link href="https://www.linkedin.com/in/amritpalsinghx21/" target="_blank" className="text-gray-400 hover:text-white">
                             <FaLinkedinIn />
                         </Link>
                         <Link href="#" className="text-gray-400 hover:text-white">
@@ -32,19 +32,14 @@ const Footer = () => {
                         </Link>
                     </div>
                 </div>
-
-               
             </div>
 
-            <div className=" text-gray-400 py-1">
-                <div className="max-w-6xl mx-auto px-4 flex justify-between items-center">
-                    <span className="text-sm">
+            <div className="text-gray-400 py-4">
+                <div className="max-w-6xl mx-auto px-4 flex flex-col sm:flex-row justify-between items-center">
+                    <span className="text-sm mb-4 sm:mb-0">
                         Copyright © 2024 <Link href="/" className="text-white hover:text-blue-500">Amritpal Singh</Link>. All rights reserved
                     </span>
-                    <div className="space-x-4">
-                        <Link href="/" className="hover:text-white">Privacy policy</Link>
-                        <Link href="/" className="hover:text-white">Terms & condition</Link>
-                    </div>
+                    
                 </div>
             </div>
         </footer>
